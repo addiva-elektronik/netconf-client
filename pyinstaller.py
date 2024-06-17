98bb6fd468a6f32206c9008024f91ba02e59748e
Ejub Sabic <ejub1946@outlook.com>
2024-06-17 21:48:44 +0200

    fixed version not loading when running from shell

--- pyinstaller.py
+++ pyinstaller.py
@@ -1,20 +1,9 @@
 import os
 import PyInstaller.__main__
-import git
+import simple_netconf_client
 
 if __name__ == '__main__':
-    # Deletes current version.txt file
-    if os.path.exists('version.txt'):
-        os.remove('version.txt')
-
-    # Writes latest git version info to 'version.txt'
-    r = git.repo.Repo(search_parent_directories=True)
-    version_info = r.git.describe('--dirty', '--tags')
-    with open('version.txt', 'w') as f:
-        f.write(version_info)
-        f.write('\n')
-        f.close()
-
+    simple_netconf_client.get_version(should_regenerate_version_txt=True)
     # Builds production version (no debug console window)
     PyInstaller.__main__.run([
         'main.spec'
--- simple_netconf_client/__init__.py
+++ simple_netconf_client/__init__.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import logging
+import git
 
 def resource_path(relative_path):
     try:
@@ -24,13 +25,25 @@ def setup_logging(debug_enabled):
 
     logging.basicConfig(level=logging_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
-try:
-    version_file_path = resource_path('version.txt')
-    with open(version_file_path) as f:
-        __version__ = f.readlines()[0].strip()
-except Exception:
-    # package is not installed
-    __version__ = "0.0.0"
+def generate_version_file():
+    r = git.repo.Repo(search_parent_directories=True)
+    version_info = r.git.describe('--dirty', '--tags')
+    with open(resource_path('version.txt'), 'w') as f:
+        f.write(version_info)
+        f.write('\n')
+        f.close()
 
-def get_version():
-    return __version__
+def get_version(should_regenerate_version_txt=False):
+    try:
+        # Deletes current version.txt file
+        if os.path.exists(resource_path('version.txt')) and should_regenerate_version_txt:
+            os.remove(resource_path('version.txt'))
+        
+        if not os.path.exists(resource_path('version.txt')) or should_regenerate_version_txt:
+            generate_version_file()
+
+        version_file_path = resource_path('version.txt')
+        with open(version_file_path) as f:
+            return f.readlines()[0].strip()
+    except Exception:
+        return "0.0.0"
